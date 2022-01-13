5cfa59d4ecdaa00e632caed7c0cc4f47daeb1099
Joana Poppe <joanapoppe@gmail.com>
2022-01-13 12:48:34 -0300

    graph1-13jan

---
+++ .vscode/graph1-13jan.py
@@ -0,0 +1,17 @@
+Idade = "19"
+Sexo = female 
+IMC = 27.9 
+Filhos = 0
+Fumante = yes
+Regiao = "southwest"
+Custos = 16.884.924
+
+print ("A variável Idade tem o tipo", type(Idade), "e o valor", Idade)
+print ("A variável sexo tem o tipo", type (Sexo), "e o valor", Sexo)
+print ("A variável IMC tem o tipo", type(IMC), "e o valor", IMC)
+print ("A variável Filho tem o tipo", type(Idade), "e o valor", Filho)
+print ("A variável Fumante tem o tipo", type(Fumante), "e o valor", Fumante)
+print ("A variável Regiao tem o tipo", type(Regiao), "e o valor", Regiao)
+print ("A variável Custos tem o tipo", type(Custos), "e o valor", Custos)
+
+
