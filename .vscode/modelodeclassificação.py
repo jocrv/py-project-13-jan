3cc0ed131790ce5264568ee2b9e617c195a9c820
Joana Poppe <joanapoppe@gmail.com>
2022-01-13 18:37:34 -0300

    part1
    Signed-off-by: Joana Poppe <joanapoppe@gmail.com>

---
+++ .vscode/modelodeclassificação.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+
+
+df = sns.load_dataset("iris")
---
+++ .vscode/print('hello').py
@@ -0,0 +1,7 @@
+        {
+            "name": "Python C++ Debugger",
+            "type": "pythoncpp",
+            "request": "launch",
+            "pythonLaunchName": "Python: Current File",
+            "cppAttachName": "'(Windows) Attach' or '(gdb) Attach'"
+        },
